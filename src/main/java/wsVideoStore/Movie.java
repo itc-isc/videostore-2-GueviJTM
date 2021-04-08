5fb3fa10c280c267900f4a72b20390c1a3716772
GueviJTM <jaziel150@hotmail.com>
2021-04-08 17:07:25 -0500

    Se añadio el priceCode y categorias a movie

--- src/main/java/wsVideoStore/Movie.java
+++ src/main/java/wsVideoStore/Movie.java
@@ -3,9 +3,14 @@ package wsVideoStore;
 public abstract class Movie
 {
 	private String title;
+	private int priceCode;
 
 	public Movie(String title) {
 		this.title 		= title;
+	}
+
+	public int getPriceCode(){
+		return priceCode;
 	}
 		
 	public String getTitle () {
@@ -15,4 +20,9 @@ public abstract class Movie
   public abstract double determineAmount(int daysRented);
 
   public abstract int determineFrequentRenterPoints(int daysRented);
+
+
+  	public static final int REGULAR = 1;
+  	public static final int NEW_RELEASE = 2;
+	public static final int CHILDRENS = 3;
 }
